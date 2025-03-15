ea9837334b715e417a25484dd8be51fa5b7a6aba
Chris Brown <chris@ourbigadventure.com>
2025-03-15 00:08:54 -0400

    Reducing Swift Version to 5.10

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
   name: "appattest-swift",
   platforms: [
-    .macOS(.v14),
+    .macOS(.v12),
     .iOS(.v17),
   ],
   products: [
